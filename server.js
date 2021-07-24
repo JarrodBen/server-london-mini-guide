77e0b2c07d334989d3078d35cc9701dea40904fe
JarrodBen <jarrodb.247@gmail.com>
2021-07-24 14:36:40 +0200

    trying to fix route errors

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const data = {
    Stratford: stratford
 }
 // const harrow = require("./data/Harrow.json")
+    //const data = {
+   //Stratford: stratford
+  //}
 // const heathrow = require("./data/Heathrow.json")
 
 app.get("/",  (req, res) => {
